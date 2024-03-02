a846380845d2ce3eecc7309114a45115538e3d03
Mohamed_Sabry <mohamedamramer20@gmail.com>
2024-03-02 19:49:58 +0200

    Adding RTOS Kernel Files

---
+++ RTOS/Kernel/Kernel_prog.c
@@ -0,0 +1,18 @@
+#include "STD_TYPES.h"
+#include "ErrType.h"
+
+#include "GIE_interface.h"
+#include "TIMERS_interface.h"
+
+#include "Kernel_interface.h"
+#include "Kernel_cfg.h"
+#include "Kernel_prv.h"
+
+
+uint8 Kernel_u8Createtask(uint8 Copy_u8Priority, uint16 Copy_u16Periodicity, void(*Copy_pvTaskHandler)(void){
+	
+}
+
+void Kernel_u8StartScheduler(void){
+	
+}
